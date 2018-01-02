683a871d2b6564a65e232ee7966d2976949d915a
mu1902 <artemis_1877@163.com>
2018-01-02 10:55:47 +0800

    20180102

--- public/js/main.js
+++ public/js/main.js
@@ -5,16 +5,47 @@ jQuery(function () {
         data = res;
         data["logs"] = data["logs"].replace(/[\r\n]/g, "");
         data["_logs"] = [];
-        $.each(data["logs"].split('#').reverse(), function (i, log) {
+        data["instance"] = [];
+        $.each(data["logs"].split('#'), function (i, log) {
             // [2017-12-14T11:15:32.065] [INFO] job - success:test;ID:1513221332015;Return:test from python.
-            var instance = {};
+            var line = {};
             if (log) {
-                instance["Name"] = log.split(' - ')[1].split(';')[0].split(':')[1];
-                instance["ID"] = log.split(' - ')[1].split(';')[1].split(':')[1];
-                instance["State"] = log.split(' - ')[1].split(';')[0].split(':')[0];
-                instance["Time"] = log.split(' - ')[0].slice(0, 25) + '</td>';
-                instance["Return"] = log.split(' - ')[1].split(';').length > 2 ? log.split(' - ')[1].split(';')[2].slice(7) : "";
-                data["_logs"].push(instance);
+                line["Name"] = log.split(' - ')[1].split(';')[0].split(':')[1];
+                line["ID"] = log.split(' - ')[1].split(';')[1].split(':')[1];
+                line["State"] = log.split(' - ')[1].split(';')[0].split(':')[0];
+                line["Time"] = log.split(' - ')[0].slice(0, 25);
+                line["Return"] = log.split(' - ')[1].split(';').length > 2 ? log.split(' - ')[1].split(';')[2].slice(7) : "";
+                data["_logs"].push(line);
+            }
+        });
+
+        $.each(data["_logs"], function (i, log) {
+            if (data["instance"].length == 0) {
+                data["instance"].push({ "ID": log["ID"], "Name": log["Name"], "Start": log["Time"] })
+            } else {
+                $.each(data["instance"], function (j, ins) {
+                    if (ins["ID"] == log["ID"]) {
+                        switch (log["State"]) {
+                            case 'complete':
+                                ins['Complete'] = log['Time'];
+                                break;
+                            case 'success':
+                                ins['Success'] = log['Time'];
+                                ins['Return'] = log['Return'];
+                                break;
+                            case 'fail':
+                                ins['Fail'] = log['Time'];
+                                ins['Return'] = log['Return'];
+                                break;
+                            default:
+                                break;
+                        }
+                        return false;// 退出循环
+                    }
+                    if (j == data["instance"].length - 1) {
+                        data["instance"].push({ "ID": log["ID"], "Name": log["Name"], "Start": log["Time"] })
+                    }
+                });
             }
         });
 
@@ -40,15 +71,17 @@ function showDetail() {
     var name = $(this).data('name');
     $('#instance tr:not(:first)').remove();
 
-    for (var i in data['_logs']) {
-        if (name == data['_logs'][i]['Name']) {
+    $.each(data['instance'].reverse(), function (i, ins) {
+        if (name == ins['Name']) {
             var row = '<tr>';
-            row += '<td>' + data['_logs'][i]['ID'] + '</td>';
-            row += '<td>' + data['_logs'][i]['State'] + '</td>';
-            row += '<td>' + data['_logs'][i]['Time'] + '</td>';
-            row += '<td>' + data['_logs'][i]['Return'] + '</td>';
+            row += '<td>' + (ins['ID'] || '') + '</td>';
+            row += '<td>' + (ins['Start'] || '') + '</td>';
+            row += '<td>' + (ins['Complete'] || '') + '</td>';
+            row += '<td>' + (ins['Success'] || '') + '</td>';
+            row += '<td>' + (ins['Fail'] || '') + '</td>';
+            row += '<td>' + (ins['Return'] || '') + '</td>';
             row += '</tr>';
             $(row).appendTo($('#instance'));
         }
-    }
+    })
 }
